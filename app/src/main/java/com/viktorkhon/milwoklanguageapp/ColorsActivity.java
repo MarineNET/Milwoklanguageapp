44d54fdfe9894c04346f9b1f2041925217a48546
Viktor Khon <betepokus@yahoo.com>
2017-06-26 23:40:21 -0400

    Add words for Colors

--- app/src/main/java/com/viktorkhon/milwoklanguageapp/ColorsActivity.java
+++ app/src/main/java/com/viktorkhon/milwoklanguageapp/ColorsActivity.java
@@ -2,6 +2,9 @@ package com.viktorkhon.milwoklanguageapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
 
@@ -9,5 +12,35 @@ public class ColorsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.word_list);
+
+        // Create a new string of words in an ArrayList from a Word object
+        ArrayList<Word> words = new ArrayList<>();
+
+        words.add(new Word("red", "weṭeṭṭi"));
+        words.add(new Word("green", "chokokki"));
+        words.add(new Word("brown", "ṭakaakki"));
+        words.add(new Word("gray", "ṭopoppi"));
+        words.add(new Word("black", "kululli"));
+        words.add(new Word("white", "kelelli"));
+        words.add(new Word("dusty yellow", "ṭopiisә"));
+        words.add(new Word("mustard yellow", "chiwiiṭә"));
+
+
+        /** Create a {@link WordAdapter}, whose data source is a list of {@link Word}s. The
+         * adapter knows how to create a list items for each item in the list
+         */
+        WordAdapter adapter = new WordAdapter(this, words);
+
+        /** Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+         * There should be a {@link ListView} with the view ID called list, which is declared in
+         * word_list.xml file.
+         */
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        /** Make the {@link ListView} use the {@link WordAdapter} we created above, so
+         * that the {@link ListView} will display list items for each {@link Word} in the list.
+         */
+        listView.setAdapter(adapter);
+
     }
 }
