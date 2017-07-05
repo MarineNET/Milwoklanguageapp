cd8c0d71d5144cac84a202a66061d7ceac906614
Viktor Khon <betepokus@yahoo.com>
2017-07-05 15:58:51 -0400

    Use ArrayList to populate NumbersActivity file

--- app/src/main/java/com/viktorkhon/milwoklanguageapp/NumbersActivity.java
+++ app/src/main/java/com/viktorkhon/milwoklanguageapp/NumbersActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
+
 public class NumbersActivity extends AppCompatActivity {
 
 
@@ -12,6 +14,18 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+
+        Log.v("Number one: ", words.get(0));
+        Log.v("Number two: ", words.get(1));
+        Log.v("Number three: ", words.get(2));
+        Log.v("Number four: ", words.get(3));
+        Log.v("Number five: ", words.get(4));
     }
 
 
