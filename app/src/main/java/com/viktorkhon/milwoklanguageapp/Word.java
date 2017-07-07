13b140d464621584093d6dd4c3cf43c502dc3e7e
Viktor Khon <betepokus@yahoo.com>
2017-07-07 00:32:11 -0400

    Overload constructor in Word class

--- app/src/main/java/com/viktorkhon/milwoklanguageapp/Word.java
+++ app/src/main/java/com/viktorkhon/milwoklanguageapp/Word.java
@@ -7,10 +7,17 @@ package com.viktorkhon.milwoklanguageapp;
 public class Word {
     private String mDefaultTranslation;
     private String mMiwokTranslation;
+    private int mImage;
 
     public Word(String defaultTranslation, String miwokTranslation) {
         mMiwokTranslation = miwokTranslation;
         mDefaultTranslation = defaultTranslation;
+    }
+
+    public Word(String defaultTranslation, String miwokTranslation, int image) {
+        mMiwokTranslation = miwokTranslation;
+        mDefaultTranslation = defaultTranslation;
+        mImage = image;
     }
 
     public String getMiwokTranslation() {
@@ -19,5 +26,9 @@ public class Word {
 
     public String getDefaultTranslation() {
         return mDefaultTranslation;
+    }
+
+    public int getImage() {
+        return mImage;
     }
 }
