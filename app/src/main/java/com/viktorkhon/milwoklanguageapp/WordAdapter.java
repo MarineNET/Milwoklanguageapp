ce95d8d06da2850c3e4c843729fa246b18bb6e20
Viktor Khon <betepokus@yahoo.com>
2017-07-07 00:25:34 -0400

    Add an image view to a list_item.xml and download all images

--- app/src/main/java/com/viktorkhon/milwoklanguageapp/WordAdapter.java
+++ app/src/main/java/com/viktorkhon/milwoklanguageapp/WordAdapter.java
@@ -1,9 +1,6 @@
 package com.viktorkhon.milwoklanguageapp;
 
 import android.content.Context;
-import android.support.annotation.LayoutRes;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,7 +8,6 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by Viktor Khon on 7/5/2017.
